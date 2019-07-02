4c24cb701d82503073a5640b7ec0586baeaffdd8
JesusGarcia15 <52469539+JesusGarcia15@users.noreply.github.com>
2019-07-02 17:03:42 -0500

    Add files via upload

---
+++ Practica 4 (Jupyter Notebook).py
@@ -0,0 +1,15 @@
+print('Hello Word')
+name = str(input("Introduce tu nombre: "))
+age = int(input("Introduce tu edad: "))
+print('Tu nombre es: ', name)
+print('Tu edad es: ', age)
+num0 = int(input("Ingrese un numero: "))
+num1 = int(input("Ingrese un segundo numero: "))
+print('El resultado de', num0,'+', num1,'es: ', num0 + num1)
+numR = float(input("Ingrese un numero real"))
+print('La raiz cuadrada de', numR, 'es: ', numR**0.5)
+from math import pi
+rad = float(input("Ingrese el radio de circulo: "))
+AC = pi * rad**2
+print('El area del circulo es: ', AC)
+input ("Presione INTRO para finalizar...")
