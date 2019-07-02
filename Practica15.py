9619b95e0583bcb99858a6e2daef6b1ccfb2bd68
JesusGarcia15 <52469539+JesusGarcia15@users.noreply.github.com>
2019-07-02 16:03:36 -0500

    Add files via upload
    Impresion de error si se ingresa un numero fuera del rango

---
+++ Practica15.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        Lista = [1, 2, 3, 4, 5]
+        print(Lista [10])
+    except IndexError:
+        print('Esta solicitando un numero que esta fuera de rango, coloque un numero entre 1-5')
+        break
+input('Puse INTRO para continuar...')
