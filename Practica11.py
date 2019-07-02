e18738baf633829a353b408fe0af4f4c3c2e32c1
JesusGarcia15 <52469539+JesusGarcia15@users.noreply.github.com>
2019-07-02 15:59:18 -0500

    Add files via upload
    Programa que imprime si un numero es par o impar

---
+++ Practica11.py
@@ -0,0 +1,7 @@
+def Comparacion(Num):
+    if Num % 2 == 0:
+        print("El numero ", Num, "es par")
+    elif Num % 2 != 0:
+        print("El numero ", Num, "es impar")
+print(Comparacion(7))
+input("Presione INTRO para finalizar...")
