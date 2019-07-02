5b1eb1f234e99a3ae0d663f1c02613e40d97a4a2
JesusGarcia15 <52469539+JesusGarcia15@users.noreply.github.com>
2019-07-02 15:40:25 -0500

    Add files via upload

---
+++ Practica1.py
@@ -0,0 +1,7 @@
+nombre = str(input("¿Como te llamas? "))
+if nombre == "Cristian" or nombre == "Pedro":
+#elif nombre == "Pedro":
+    print('Hello', nombre)
+else:
+    print('No eres nadie')
+input("pulse INTRO para finalizar...")
