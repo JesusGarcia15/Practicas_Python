fa09ea0acf3bc74ac88d80a89261c853971ada52
JesusGarcia15 <52469539+JesusGarcia15@users.noreply.github.com>
2019-07-02 15:55:58 -0500

    Add files via upload
    Impresion de mensaje con funciones

---
+++ Practica9.py
@@ -0,0 +1,13 @@
+#Paradigmas de programación TAREA!!!!!!!!
+#Metodo de biseccion
+#Metodo Newton-Raphson
+def Producto(num_1,num_2):
+    return num_1 * num_2
+    #pass#Da una estrurctura en el programa pero no hace nada
+    #break#Rompe el ciclo donde lo colocas
+print(Producto(3,4))
+#1Hacer una funcion con hola mundo 2Sumar dos numeros 3Es par o imprar el numero 4Calcule el factorial 5Funcion que dados 2 numeros muestre los que hay entre ellos
+def Impresion():
+    return "Hola mundo"
+print(Impresion())
+input("Presione INTRO para finalizar...")
