9fc17aa8c65122f77276870244017cb1d2cb1e0c
JesusGarcia15 <52469539+JesusGarcia15@users.noreply.github.com>
2019-07-02 15:57:11 -0500

    Add files via upload
    Comparación de un valor ingresado si es par o impar

---
+++ Practica10.py
@@ -0,0 +1,7 @@
+
+def Producto(num_1,num_2):
+    return num_1 + num_2
+num_1 = int(input("Ingrese un numero: "))
+num_2 = int(input("Ingrese un segundo numero: "))
+print("El resultado",num_1 ,"+",num_2 ,"=", Producto(num_1,num_2))
+input("Presione INTRO para finalizar...")
